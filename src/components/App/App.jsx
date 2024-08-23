040ebd3f7946069e76e19b3c11127a5237763357
Your NameNastya-20 <asyataa2016@gmail.com>
2024-08-24 01:42:41 +0300

    add

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,31 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import initialContacts from '../../contacts.json';
+import { useState } from 'react';
+import 'react-dom';
 import css from './App.module.css';
 import ContactForm from '../ContactForm/ContactForm';
 import SearchBox from '../SearchBox/SearchBox';
 import ContactList from '../ContactList/ContactList';
 
+
 export default function App() {
-    // Функція для отримання контактів з локального сховища
-    const getInitialContacts = () => {
-        const savedContacts = localStorage.getItem('contacts');
-        return savedContacts ? JSON.parse(savedContacts) : [];
-    };
-
-    // Ініціалізація стану контактів за допомогою функції getInitialContacts
-    const [contacts, setContacts] = useState(getInitialContacts);
+    const [contacts, setContacts] = useState(initialContacts);
     const [search, setSearch] = useState('');
-
-    // Зберігання контактів у локальному сховищі при їх зміні
-    useEffect(() => {
-        localStorage.setItem('contacts', JSON.stringify(contacts));
-    }, [contacts]);
 
     const addContact = (newContact) => { 
         setContacts((prevContacts) => [...prevContacts, newContact]);
     };
     
     const deleteContact = (contactId) => {
-        setContacts((prevContacts) =>
+        setContacts((prevContacts) => 
             prevContacts.filter((contact) => contact.id !== contactId)
         );
     };
@@ -43,3 +34,4 @@ export default function App() {
         </div>
     );
 }
+
