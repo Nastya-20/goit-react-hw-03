61018f01255e5d35f16b3159795c99f2d53c74be
Your NameNastya-20 <asyataa2016@gmail.com>
2024-08-24 12:22:02 +0300

    add

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,10 +3,10 @@ import css from './App.module.css';
 import ContactForm from '../ContactForm/ContactForm';
 import SearchBox from '../SearchBox/SearchBox';
 import ContactList from '../ContactList/ContactList';
-import initialContacts from '../../contacts.json'; 
+import initialContacts from '../../contacts.json';
 
 export default function App() {
-    // Зчитування контактів з локального сховища або початкове значення
+     // Зчитування контактів з локального сховища або початкове значенняon
     const fetchInitialContacts = () => {
         const savedContacts = localStorage.getItem('contacts');
         return savedContacts ? JSON.parse(savedContacts) : initialContacts;
@@ -15,15 +15,32 @@ export default function App() {
     const [contacts, setContacts] = useState(fetchInitialContacts);
     const [search, setSearch] = useState('');
 
-    // Зберігання контактів в локальному сховищі при їх зміні
+   // Зберігання контактів в локальному сховищі при їх зміні
     useEffect(() => {
-        localStorage.setItem('contacts', JSON.stringify(contacts));
+       localStorage.setItem('contacts', JSON.stringify(contacts));
     }, [contacts]);
 
-    const addContact = (newContact) => { 
+    useEffect(() => {
+        const savedContacts = localStorage.getItem('contacts');
+        if (!savedContacts) {
+            localStorage.setItem('contacts', JSON.stringify(initialContacts));
+        }
+    }, []);
+
+    const addContact = (newContact) => {
+        const isDuplicate = contacts.some(
+            (contact) =>
+                contact.name.toLowerCase() === newContact.name.toLowerCase() ||
+                contact.number === newContact.number
+        );
+        if (isDuplicate) {
+            alert(`${newContact.name} or ${newContact.number} is already in contacts.`);
+            return;
+        }
+
         setContacts((prevContacts) => [...prevContacts, newContact]);
     };
-    
+
     const deleteContact = (contactId) => {
         setContacts((prevContacts) =>
             prevContacts.filter((contact) => contact.id !== contactId)
@@ -43,5 +60,6 @@ export default function App() {
         </div>
     );
 }
+
 
 
